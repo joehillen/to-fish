3fc0eeefe432a9a0cee78ccc5f0b0dc64d00ff2b
Joe Hillenbrand <joehillen@gmail.com>
2015-11-11 15:51:58 -0800

    replace tabs with spaces

---
+++ README.md
@@ -0,0 +1,37 @@
+# to-fish
+
+A bookmarking tool for Fish shell. This is a [fundle](https://github.com/tuvistavie/fundle) package.
+
+## Installation
+
+### Using [fundle](https://github.com/tuvistavie/fundle) (recommended)
+
+Add
+
+```
+fundle plugin 'joehillen/to-fish'
+```
+
+to your `config.fish` and run `fundle install`.
+
+### Manually
+
+Put `functions/to.fish` in `~/.config/fish/functions/` directory,
+and put `completions/to.fish` in `~/.config/fish/completions/`.
+
+## Usage
+
+`to` puts bookmarks in the directory `~/.tofish`
+
+```
+$ to help
+Usage:
+ to <bookmark>              # Go to <bookmark>
+ to add <bookmark>          # Create a new bookmark with name <bookmark>
+                            # that points to the current directory
+ to rm <bookmark>           # Remove <bookmark>
+ to (ls|list)               # List all bookmarks
+ to (mv|rename) <old> <new> # Change the name of a bookmark
+                            # from <old> to <new>
+ to help                    # Show this message
+```
--- functions/to.fish
+++ functions/to.fish
@@ -8,21 +8,22 @@ function __to_usage
   echo ' to (ls|list)               # List all bookmarks'
   echo ' to (mv|rename) <old> <new> # Change the name of a bookmark'
   echo '                            # from <old> to <new>'
+  echo ' to help                    # Show this message'
   return 1
 end
 
 
 function to -d 'Bookmarking system.'
-	set -l tofishdir ~/.tofish
+  set -l tofishdir ~/.tofish
 
-	# Create tofish directory
-	if not test -d "$tofishdir"
-		if mkdir "$tofishdir"
-			echo "Created bookmark directory '$tofishdir'."
-		else
-			echo "Failed to Create bookmark directory '$tofishdir'."
-			return 1
-		end
+  # Create tofish directory
+  if not test -d "$tofishdir"
+    if mkdir "$tofishdir"
+      echo "Created bookmark directory '$tofishdir'."
+    else
+      echo "Failed to Create bookmark directory '$tofishdir'."
+      return 1
+    end
   end
 
   if test (count $argv) -lt 1
@@ -30,64 +31,67 @@ function to -d 'Bookmarking system.'
   end
 
   # Catch usage errors
-	switch $argv[1]
-		case add rm
-			if not test (count $argv) -ge 2
-				echo "Usage: to $argv[1] BOOKMARK"
-				return 1
-			end
+  switch $argv[1]
+    case add rm
+      if not test (count $argv) -ge 2
+        echo "Usage: to $argv[1] BOOKMARK"
+        return 1
+      end
 
-		case mv rename
-			if not test (count $argv) -ge 3
-				echo "Usage: to $argv[1] SOURCE DEST"
-				return 1
-			end
-	end
+    case mv rename
+      if not test (count $argv) -ge 3
+        echo "Usage: to $argv[1] SOURCE DEST"
+        return 1
+      end
+  end
 
-	switch $argv[1]
-		case add # Add a bookmark
-			if test -f "$tofishdir/$argv[2]"
-				echo "Error: The bookmark '$argv[2]' already exists."
-				echo "Use `to rm '$argv[2]'` to remove it first."
-				return 1
-			end
+  switch $argv[1]
+    case add # Add a bookmark
+      if test -f "$tofishdir/$argv[2]"
+        echo "Error: The bookmark '$argv[2]' already exists."
+        echo "Use `to rm '$argv[2]'` to remove it first."
+        return 1
+      end
 
-			echo "cd \""(pwd)"\"" > "$tofishdir/$argv[2]"
+      echo "cd \""(pwd)"\"" > "$tofishdir/$argv[2]"
 
-			echo "Added bookmark '$argv[2]'."
+      echo "Added bookmark '$argv[2]'."
 
-		case rm # Remove a bookmark
-			if rm -f "$tofishdir/$argv[2]"
-				echo "Removed bookmark '$argv[2]'."
-			else
-				echo "The bookmark '$argv[2]' does not exist."
-				return 1
-			end
+    case rm # Remove a bookmark
+      if rm -f "$tofishdir/$argv[2]"
+        echo "Removed bookmark '$argv[2]'."
+      else
+        echo "The bookmark '$argv[2]' does not exist."
+        return 1
+      end
 
-		case ls list # List all bookmarks
-			ls "$tofishdir"
+    case ls list # List all bookmarks
+      for b in (ls -1 $tofishdir)
+         echo ">> $b"
+         cat $tofishdir/$b
+      end
 
-		case mv rename # Rename a bookmark
-			if not test -f "$tofishdir/$argv[2]"
-				echo "The bookmark '$argv[2]' does not exist."
-				return 1
-			else if test -f "$tofishdir/$argv[3]"
-				echo "Error: The destination bookmark '$argv[3]' already exists."
-				echo "Use `to rm '$argv[3]'` to remove it first."
-				return 1
-			end
+    case mv rename # Rename a bookmark
+      if not test -f "$tofishdir/$argv[2]"
+        echo "The bookmark '$argv[2]' does not exist."
+        return 1
+      else if test -f "$tofishdir/$argv[3]"
+        echo "Error: The destination bookmark '$argv[3]' already exists."
+        echo "Use `to rm '$argv[3]'` to remove it first."
+        return 1
+      end
 
-			mv "$tofishdir/$argv[2]" "$tofishdir/$argv[3]"
+      mv "$tofishdir/$argv[2]" "$tofishdir/$argv[3]"
 
     case help
       __to_usage
 
     case '*'
-			if test -f "$tofishdir/$argv[1]"
-				. "$tofishdir/$argv[1]"
-			else
-				echo "The bookmark '$argv[1]' does not exist."
-				return 1
-			end
-	end
+      if test -f "$tofishdir/$argv[1]"
+        . "$tofishdir/$argv[1]"
+      else
+        echo "The bookmark '$argv[1]' does not exist."
+        return 1
+      end
+  end
 end
