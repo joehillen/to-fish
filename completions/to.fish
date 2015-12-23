c2fb39eaefd49165c99dc07ce31e7ab8f1738d83
Joe Hillenbrand <joehillen@gmail.com>
2015-12-23 11:37:08 -0800

    reuse __to_update_bookmark_completions

--- completions/to.fish
+++ completions/to.fish
@@ -8,8 +8,4 @@ complete -c to -n '__fish_use_subcommand' -x -a "mv" -d 'Rename a bookmark'
 complete -c to -n '__fish_use_subcommand' -x -a "rename" -d 'Rename a bookmark'
 complete -c to -n '__fish_use_subcommand' -x -a "help" -d 'Show Help'
 
-if test -d "$tofishdir"
-  for b in (ls -a1 $tofishdir | grep -xv '.' | grep -xv '..')
-    complete -c to -f -a "$b" -d 'Bookmark'
-  end
-end
+__to_update_bookmark_completions
