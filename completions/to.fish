5eae745ddca2cdb0179e8617b0d89bb986216a80
Joe Hillenbrand <joehillen@gmail.com>
2020-05-28 18:34:13 -0700

    TOTAL REFACTOR

--- completions/to.fish
+++ completions/to.fish
@@ -1,9 +1,1 @@
-complete -c to -n '__fish_use_subcommand' -x -a "add" -d 'Create a bookmark'
-complete -c to -n '__fish_use_subcommand' -x -a "rm" -d 'Remove a bookmark'
-complete -c to -n '__fish_use_subcommand' -x -a "ls" -d 'Lists all bookmarks'
-complete -c to -n '__fish_use_subcommand' -x -a "list" -d 'Lists all bookmarks'
-complete -c to -n '__fish_use_subcommand' -x -a "mv" -d 'Rename a bookmark'
-complete -c to -n '__fish_use_subcommand' -x -a "rename" -d 'Rename a bookmark'
-complete -c to -n '__fish_use_subcommand' -x -a "help" -d 'Show Help'
-
 __to_update_bookmark_completions
